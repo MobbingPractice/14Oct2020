2b4593c8e24e7be79d890ff46d1926873425bb11
enlea <enza.leano@gmail.com>
2020-10-14 19:27:07 +0200

    cart and quantity

--- src/main/java/Cart.java
+++ src/main/java/Cart.java
@@ -1,9 +1,21 @@
-public class Cart {
-    public void add(Apple apple, int quantity) {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Cart {
+	
+	List<Apple> items = new ArrayList<Apple>();
+    
+	public void add(Apple apple, int quantity) {
+    	for(int i=0; i<quantity; i++)
+    		items.add(apple);
     }
 
     public double getTotalPrice() {
-        return 50;
+    	double totalPrice = 0;
+    	
+    	for(Apple a: items) {
+    		totalPrice+=a.getPrice();
+    	}
+        return totalPrice;
     }
 }
--- src/test/java/CartTest.java
+++ src/test/java/CartTest.java
@@ -2,7 +2,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class SampleTest {
+public class CartTest {
 
     @Test
     public void testAddOneApple(){
